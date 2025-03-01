d3eff593296633a956709a7c76092bd7507c1814
ced3j <spoonnoisthere@gmail.com>
2025-03-01 22:45:28 +0300

    project end

--- flutter_101/lib/main.dart
+++ flutter_101/lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Projesi', 
+      title: 'Flutter 101 Project', 
       debugShowCheckedModeBanner: false, // Debug etiketi 
       home: const TextLearnView(),
       
